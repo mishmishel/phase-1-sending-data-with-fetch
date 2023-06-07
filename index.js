f5c1687eaeab280f0ec2f8c7db91d4e203576eee
Mishel Tyurkova <misheltyurkova@gmail.com>
2023-06-07 10:01:09 +1000

    Finished Lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Add your code here
+const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify({
+      name: "Steve",
+      email: "steve@steve.com",
+    }),
+};
+
+function submitData() {
+    return fetch("http://localhost:3000/users", configurationObject)
+    .then(function (response) {
+        return response.json();
+      })
+      .then(function (object) {
+        console.log(object.id)
+        document.body.innerHTML += object.id;
+      })
+      .catch(function (error) {
+        alert("Error");
+        console.log(error.message)
+        document.body.innerHTML += error.message;
+      });
+}
+  
+
+
